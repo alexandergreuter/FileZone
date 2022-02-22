c4692fd617e82566bea88da588689529591d71a9
RaviAnand Mohabir <moravrav@gmail.com>
2022-02-22 14:28:12 +0100

    feat(webapi): :fire: Remove Unnecessary HTTP Methods from `UploadsController`
    Remove edit and delete functionality as well as get all to avoid unauthorized access to uploads.

--- api/Controllers/UploadsController.cs
+++ api/Controllers/UploadsController.cs
@@ -17,13 +17,6 @@ namespace FileZone.Controllers
             _uploadService = uploadService;
         }
 
-        // GET: api/files
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Upload>>> GetUploads()
-        {
-            return await _uploadService.GetUploads();
-        }
-
         // GET: api/files/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Upload>> GetUpload(long id)
@@ -38,26 +31,6 @@ namespace FileZone.Controllers
             return file;
         }
 
-        // PUT: api/files/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUpload(long id, [FromBody] Upload file)
-        {
-            if (id != file.Id)
-            {
-                return BadRequest();
-            }
-
-            var f = await _uploadService.EditUpload(id, file);
-
-            if (f == null)
-            {
-                return NotFound();
-            }
-
-            return NoContent();
-        }
-
         // POST: api/files
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
@@ -66,21 +39,6 @@ namespace FileZone.Controllers
             var f = await _uploadService.CreateUpload(file);
 
             return CreatedAtAction(nameof(GetUpload), new { id = f.Id }, f);
-        }
-
-        // DELETE: api/files/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUpload(long id)
-        {
-            var file = await _uploadService.GetUpload(id);
-            if (file == null)
-            {
-                return NotFound();
-            }
-
-            _uploadService.DeleteUpload(id);
-
-            return NoContent();
         }
     }
 }
