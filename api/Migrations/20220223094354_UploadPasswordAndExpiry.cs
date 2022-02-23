00f1463a38403df302135a3218f08f52a48b9a27
RaviAnand Mohabir <moravrav@gmail.com>
2022-02-23 10:45:38 +0100

    chore(webapi): :card_file_box: Generate DB Migrations.

---
+++ api/Migrations/20220223094354_UploadPasswordAndExpiry.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileZone.Migrations
+{
+    public partial class UploadPasswordAndExpiry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "title",
+                table: "uploads",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "uploads",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "expires_at",
+                table: "uploads",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "password",
+                table: "uploads",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "expires_at",
+                table: "uploads");
+
+            migrationBuilder.DropColumn(
+                name: "password",
+                table: "uploads");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "title",
+                table: "uploads",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "uploads",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
